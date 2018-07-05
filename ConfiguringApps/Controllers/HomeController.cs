582440a7bbb4abc0db342f664c1f72ff78988b0f
Łukasz Majcher <lukasz.majcherrr@gmail.com>
2018-07-05 09:25:52 +0200

    ASP.NET features, Middleware software

---
+++ ConfiguringApps/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ConfiguringApps.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConfiguringApps.Controllers
+{
+    public class HomeController : Controller
+    {
+        private UptimeService uptime;
+
+        public HomeController(UptimeService up) => uptime = up;
+        public ViewResult Index() => View(new Dictionary<string, string>
+        {
+            ["Message"] = "This is an action method called Index()",
+            ["Uptime"] = $"{uptime.Uptime}ms"
+        });
+    }
+}
